1024d3268da8bfae5f3a92dc3628d0a392d23edd
Vijay Thomas <vijay_thomas@intuit.com>
2020-06-26 20:22:07 -0700

    Finally removing debug statements

--- chaos-test/chaostest/chaostest/utils/report.py
+++ chaos-test/chaostest/chaostest/utils/report.py
@@ -34,8 +34,6 @@ class Report(object):
 
         output_data = {}
         # Get namespace, appEndpoint, label and application from serializer
-        print("Reached json parser")
-        print(serializer)
         output_data['action'] = serializer['EXP']
         output_data['appEndpoint'] = serializer['APP_ENDPOINT']
         output_data['custom'] = {}
